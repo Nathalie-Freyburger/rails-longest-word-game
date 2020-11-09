844d9668c9c5d080f59298706d1727d1d1cd4716
Nathalie Freyburger <nfreyburger.pers0@gmail.com>
2020-11-09 16:01:51 +0100

    rails update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,11 @@
 class GamesController < ApplicationController
     
     def new
+      @letters = Array.new(10) { ('A'..'Z').to_a.sample }
     end
 
     def score
+        @attempt = params[:your_try]
     end
+
 end
